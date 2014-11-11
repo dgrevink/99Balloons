359291bbf58a4712cd05006bbd05efed3d8cb3cc
David Grevink <dgrevink@starplace.org>
2014-11-10 23:45:08 -0500

    Latest changes.

--- 99Balloons/ViewController.swift
+++ 99Balloons/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    var currentIndex = 0
+    var balloonCounter = 99
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextButtonPressed(sender: AnyObject) {
+        // process random image
+        var randomIndex: Int
+        do {
+            randomIndex = Int(arc4random_uniform(UInt32(4)))
+        } while currentIndex == randomIndex
+        currentIndex = randomIndex
+        mainImage.image = UIImage(named: "RedBalloon\(randomIndex+1).jpg" );
+
+        // process backward counter
+        if ( balloonCounter == 0 ) {
+            balloonCounter = 99
+        }
+        nameLabel.text = "\(balloonCounter) Balloons"
+        balloonCounter--
+    }
 
 }
 
